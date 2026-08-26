613e5aa12e8f2d0be583f609bf57234d6adb9de8
Melisa Celik <257322652+melisaceliklabs@users.noreply.github.com>
2026-08-26 22:29:02 +0300

    Günlük Ciro

---
+++ gwz_sales.sql
@@ -0,0 +1,9 @@
+-- Günlük ciro (promosyon sonrası net ciro)
+-- Kaynak: data-analytics-469406.course14.gwz_sales
+
+SELECT
+    date_date,
+    ROUND(SUM(turnover), 2) AS daily_turnover
+FROM `data-analytics-469406.course14.gwz_sales`
+GROUP BY date_date
+ORDER BY date_date
