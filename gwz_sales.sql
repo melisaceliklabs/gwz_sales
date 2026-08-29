1aedcccc40afffc1381dc7db93b301e34274129e
Melisa Celik <257322652+melisaceliklabs@users.noreply.github.com>
2026-08-29 22:13:00 +0300

    sort dates newest first

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,10 +1,10 @@
--- Günlük ciro (promosyon sonrası net ciro)
+-- Günlük ciro ve satın alma maliyeti
 -- Kaynak: data-analytics-469406.course14.gwz_sales
 
 SELECT
     date_date,
     ROUND(SUM(turnover), 2) AS daily_turnover,
     SUM(purchase_cost) AS purchase_cost
-FROM `data-analytics-469406.course14.gwz_sales`
+FROM \`data-analytics-469406.course14.gwz_sales\`
 GROUP BY date_date
-ORDER BY date_date
+ORDER BY date_date DESC
